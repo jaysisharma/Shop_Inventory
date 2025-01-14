34bae6f51128e609b740265fa01601f5dd0db64b
jaysi <jaysi@kali.jaysi>
2025-01-14 11:01:56 +0530

    file added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://jaisysharma9817:cwGTQ8YB7AYQ7zI7@cluster0.xzq90.mongodb.net/?retryWrites=true&w=majority&tls=true&appName=Cluster0",{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
